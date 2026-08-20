f97595480c8f41964a69b076a7c1debb94ceade5
teguhsms05 <teguhsms05@users.noreply.github.com>
2026-08-20 11:33:50 +0700

    update color

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,7 +1,7 @@
 import multiprocessing
 import os
 
-bind = f"{os.environ.get('APP_HOST', '0.0.0.0')}:{os.environ.get('APP_PORT', '5010')}"
+bind = f"{os.environ.get('APP_HOST', '0.0.0.0')}:{os.environ.get('APP_PORT', '5050')}"
 workers = multiprocessing.cpu_count() * 2 + 1
 worker_class = "sync"
 timeout = 120
